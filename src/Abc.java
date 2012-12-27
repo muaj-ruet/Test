2371209d0b04fcca50937802195e4f7858bf8edb
user_2 <user_2@sw-12>
2012-12-27 12:50:59 +0600

    first

---
+++ src/Abc.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user_2
+ */
+public class Abc {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    int radius=3;
+    double area;
+    area=3.14*radius*radius;
+    System.out.println("Area"+ area);
+    }
+}
